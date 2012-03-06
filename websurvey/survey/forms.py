e48f81ced0b30e06a3f6bb4d21485b8efa6cf12c
Trevor Oakes <trevor.oakes@gmail.com>
2012-03-06 01:25:53 -0700

    Finished requirements

--- websurvey/survey/forms.py
+++ websurvey/survey/forms.py
@@ -18,7 +18,7 @@ H_GAME = (('Gradius V', 'Gradius V'),
           ('Sexy Parodius', 'Sexy Parodius'),
           ('Border Down', 'Border Down'),
           ('Deathsmiles', 'Deathsmiles'),
-          ('Your Choice: ', 'Other'))
+          ('Your choice: ', 'Other'))
 
 DEVELOPER = (('Cave', 'Cave'),
              ('Treasure', 'Treasure'))
@@ -34,29 +34,86 @@ TREASURE = (('Radiant Silvergun', 'Radiant Silvergun'),
             ('Sin and Punishment: Star Successor', 'Sin and Punishment: Star Successor'),
             ('Your choice: ', 'Other'))
 
+
+class ControllerForm(forms.Form):
+    controller = forms.ChoiceField(widget=RadioSelect, choices=CONTROLLERS, label='Favorite Controller?')
+
+class GateForm(forms.Form):
+    gate = forms.ChoiceField(widget=RadioSelect, choices=GATES, label='Favorite arcade stick gate?')
+
+class ScrollingForm(forms.Form):
+    scrolling = forms.ChoiceField(widget=RadioSelect, choices=SCROLLING, label='Do you like vertical or horizontal scrollers better?')
+
+class HGameForm(forms.Form):
+    game = forms.ChoiceField(widget=RadioSelect, choices=H_GAME, label='What is your favorite horizontal scroller?')
+    other = forms.CharField(required=False, label='Other:')
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        game = cleaned_data.get("game")
+        other = cleaned_data.get("other")
+        if game:
+            if "Your choice:" in game and len(other) == 0:
+                raise forms.ValidationError("If you choose Other, you must type in your choice.")
+        return cleaned_data
+
+class VDeveloperForm(forms.Form):
+    vdeveloper = forms.ChoiceField(widget=RadioSelect, choices=DEVELOPER, label='Whose vertical scrolling games do you prefer?')
+
+class VCaveForm(forms.Form):
+    game = forms.ChoiceField(widget=RadioSelect, choices=CAVE, label='What is your favorite game made by Cave?')
+    other = forms.CharField(required=False, label='Other:')
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        game = cleaned_data.get("game")
+        other = cleaned_data.get("other")
+        if game:
+            if "Your choice:" in game and len(other) == 0:
+                raise forms.ValidationError("If you choose Other, you must type in your choice.")
+        return cleaned_data
+
+class VTreasureForm(forms.Form):
+    game = forms.ChoiceField(widget=RadioSelect, choices=TREASURE, label='What is your favorite game made by Treasure?')
+    other = forms.CharField(required=False, label='Other:')
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        game = cleaned_data.get("game")
+        other = cleaned_data.get("other")
+        if game:
+            if "Your choice:" in game and len(other) == 0:
+                raise forms.ValidationError("If you choose Other, you must type in your choice.")
+        return cleaned_data
+
+
+FORMS = [ControllerForm, GateForm, ScrollingForm, HGameForm, VDeveloperForm,
+         VCaveForm, VTreasureForm]
+
 class SurveyWizard(FormWizard):
     def done(self, request, form_list):
         return render_to_response('results.html', {
             'form_data': [form.cleaned_data.iteritems() for form in form_list],
         })
-
-class ControllerForm(forms.Form):
-    controller = forms.ChoiceField(widget=RadioSelect, choices=CONTROLLERS)
-
-class GateForm(forms.Form):
-    gate = forms.ChoiceField(widget=RadioSelect, choices=GATES)
-
-class ScrollingForm(forms.Form):
-    scrolling = forms.ChoiceField(widget=RadioSelect, choices=SCROLLING)
-
-class HGameForm(forms.Form):
-    hgame = forms.ChoiceField(widget=RadioSelect, choices=H_GAME)
-
-class VDeveloperForm(forms.Form):
-    vdeveloper = forms.ChoiceField(widget=RadioSelect, choices=DEVELOPER)
-
-class VCaveForm(forms.Form):
-    vgame = forms.ChoiceField(widget=RadioSelect, choices=CAVE)
-
-class VTreasureForm(forms.Form):
-    vgame = forms.ChoiceField(widget=RadioSelect, choices=TREASURE)
+    def process_step(self, request, form, step):
+        if hasattr(form, 'cleaned_data'):
+            self.form_list = self.form_list[0:step+1]
+            if form.cleaned_data.get('controller', None) is not None:
+                self.form_list.extend(FORMS[1:])
+                if form.cleaned_data['controller'] != 'Arcade Stick':
+                    self.form_list.pop(step+1)
+            if form.cleaned_data.get('gate', None) is not None:
+                self.form_list.extend(FORMS[2:])
+            if form.cleaned_data.get('scrolling', None) is not None:
+                self.form_list.extend(FORMS[3:])
+                if form.cleaned_data['scrolling'] == 'Horizontal':
+                    self.form_list.pop(step+2)
+                    self.form_list.pop(step+2)
+                    self.form_list.pop(step+2)
+                else:
+                    self.form_list.pop(step+1)
+            if form.cleaned_data.get('vdeveloper', None) is not None:
+                self.form_list.extend(FORMS[5:])
+                if form.cleaned_data['vdeveloper'] == 'Cave':
+                    self.form_list.pop(step+2)
+                else:
+                    self.form_list.pop(step+1)
